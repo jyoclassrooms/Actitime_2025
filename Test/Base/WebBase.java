f18b9633a6871f76e10edca22e159efb5c9b7020
LENOVO <LENOVO@PC-f7ad11>
2025-01-07 11:25:04 +0530

    Framework_v1

---
+++ Test/Base/WebBase.java
@@ -0,0 +1,36 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
+import org.testng.annotations.BeforeMethod;
+
+public class WebBase 
+{
+	 WebDriver driver=null;
+	 String browser="chrome";
+	 String url="http://127.0.0.1/login.do";
+	 @BeforeMethod
+	 public void login()
+	 {
+		 switch( browser)
+		 {
+		    case "chrome":
+		    	driver=new ChromeDriver();
+		        break;
+		    case "edge":
+		    	driver=new EdgeDriver();
+		    	break;
+		    case "firefox":
+		    	driver=new FirefoxDriver();
+		    	break;
+		    	
+		 }
+		 
+		 driver.get(url);
+		 
+	 }
+
+}
---
+++ Test/pageobject/Time_Track.java
@@ -0,0 +1,5 @@
+package pageobject;
+
+public class Time_Track {
+
+}
